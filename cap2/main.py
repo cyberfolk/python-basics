4300ab11be09a211190f04556918ceb4a9831c20
cyberfolk <andrea1995ferrari@gmail.com>
2023-08-19 22:26:55 +0200

    Add code in cap2

---
+++ cap2/main.py
@@ -0,0 +1,24 @@
+a, b = 9, 3
+print(type(a))
+
+c = -34
+d = 9768.23
+
+f = True
+g = False
+
+e = [1, 2, 3]  # list
+h = (1, 2, 3)  # tuple
+
+i = range(3)  # [1, 2, 3]
+
+l = {1, 2, 3}  # set
+
+m = {"name": "luca", "lastname": "rossi", "ID": "001"}  # dict{k,v}
+
+n = input("What's your name? ")
+print("Hi " + n)
+
+o = 5
+p = "5"
+print(o + int(p))   # casting to int
