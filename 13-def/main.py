09485a502f1d6e1abcce78ccaa567228076a2a41
cyberfolk <andrea1995ferrari@gmail.com>
2023-08-20 10:14:06 +0200

    13. Add Arbitrary Arguments

--- 13-def/main.py
+++ 13-def/main.py
@@ -6,7 +6,18 @@ def simple_sum(a, b):
     return a + b
 
 
+def loop_sum(*operators):   # Arbitrary Arguments
+    acc = 0
+    for operator in operators:
+        acc += operator
+    return acc
+
+
 greet("Raffaele")
 print(simple_sum(1, 2))
 print(simple_sum(1, 3))
 print(simple_sum(1, 5))
+
+print(loop_sum(1, 2, 3, 4, 5, 6, 7, 8, 9, ))
+
+# Arbitrary Keyword Arguments (todo)
